e0e879013428cf4081072c9e80bb3a9635311c70
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-09-25 10:57:52 -0300

    Feat: add Service-One project

---
+++ service-one/src/test/java/com/msrocketseat/service_one/ServiceOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msrocketseat.service_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
