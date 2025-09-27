d13d5bcc916b87586b324d45d575c2a7352e9079
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-09-27 16:51:08 -0300

    feat: add initial project structure with Maven wrapper and Spring Boot application

---
+++ service-task/src/main/java/com/msrocketseat/service_task/ServiceTaskApplication.java
@@ -0,0 +1,17 @@
+package com.msrocketseat.service_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
+public class ServiceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTaskApplication.class, args);
+	}
+
+}
