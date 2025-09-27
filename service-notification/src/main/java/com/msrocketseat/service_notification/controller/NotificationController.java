aefa8984ba5c86cf6e906965d53140e9bad831b9
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-09-27 17:36:14 -0300

    feat: add NotificationController class

---
+++ service-notification/src/main/java/com/msrocketseat/service_notification/controller/NotificationController.java
@@ -0,0 +1,19 @@
+package com.msrocketseat.service_notification.controller;
+
+import com.msrocketseat.service_notification.dto.NotificationRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+
+    @PostMapping
+    public ResponseEntity<Object> sendNotification(@RequestBody NotificationRequest notificationRequest) {
+        System.out.println(notificationRequest.message());
+        return ResponseEntity.ok().build();
+    }
+}
